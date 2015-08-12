2636c1c2c83030816914351e0a302a96c42ea71d
Chicas <chicasa@chicasa-01-mba.advisory.com>
2015-08-12 10:05:44 -0400

    Adding CSS file

---
+++ presentation/html/css.php
@@ -0,0 +1,19 @@
+<?php
+$s_css  = "<style type='text/css'>";
+$s_css .=    "body {font-family:Calibri;}";
+$s_css .=    "table {border-collapse:collapse;}";
+$s_css .=    "table,th,td {border:1px solid black;}";
+$s_css .=    "th {background-color:#e4e4e4;white-space:nowrap;padding:5px 5px 5px 5px;color:#193a49;}";
+$s_css .=    "tr {border:1px solid black;}";
+$s_css .=    "td {white-space:nowrap;padding:5px 5px 5px 5px;}";
+$s_css .=    ".thProjects {text-align:left;font-size:18;}";
+$s_css .=    ".tdDate {text-align:center;}";
+$s_css .=    ".tdEmailHeader {text-align:right;}";
+$s_css .=    ".tdStats {border:none;}";
+$s_css .=    ".tdNum {text-align:right;}";
+$s_css .=    "svg {font: 10px sans-serif;}";
+$s_css .=    ".bar rect {fill: steelblue;shape-rendering: crispEdges;}";
+$s_css .=    ".bar text {fill: #fff;}";
+$s_css .=    ".axis path,.axis line {fill: none;stroke: #000;shape-rendering: crispEdges;}";
+$s_css .= "</style>";
+?>
